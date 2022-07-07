262f58bd007e1f0b3e0e3f7187b39b2e9d1c64df
darpanpathak <pathakdarpan77@gmail.com>
2022-07-07 11:23:38 +0530

    updated logic

--- product-of-array-except-self/index.js
+++ product-of-array-except-self/index.js
@@ -2,17 +2,32 @@
 
 // For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 
-const product = (input) => {
-  const totalProduct = input.reduce((acc, val) => acc * val, 1);
+//https://leetcode.com/problems/product-of-array-except-self/
 
-  return input.map((item) => {
-    return totalProduct / item;
-  });
+const product = (input) => {
+  const outArr = new Array(input.length).fill(1);
+  let temp = 1;
+  //prefix
+  for (let i = 0; i < input.length; i++) {
+    outArr[i] = temp;
+    temp *= input[i];
+  }
+  // console.log(outArr);
+
+  temp = 1;
+  //postfix
+  for (let i = input.length - 1; i >= 0; i--) {
+    outArr[i] *= temp;
+    temp *= input[i];
+  }
+
+  return outArr;
 };
 
 const inputs = [
   [1, 2, 3, 4, 5],
   [3, 2, 1],
+  [-1, 1, 0, -3, 3],
 ];
 
 for (const input of inputs) {
